2deed2add8d57f90df0ae03a5db475852ff9995e
juliusalberto <u7724204@anu.edu.au>
2025-02-22 08:46:28 +1100

    [GREEN] test DB config on localhost

--- backend/internal/database/db_test.go
+++ backend/internal/database/db_test.go
@@ -1,8 +1,11 @@
 package database
 
 import (
-	"testing"
 	"database/sql"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
 )
 
 type TestDB struct {
@@ -10,22 +13,16 @@ type TestDB struct {
 }
 
 func SetupTestDB(t *testing.T) *TestDB {
+	if err := godotenv.Load("../../../.env.test"); err != nil {
+		t.Fatalf("Error loading .env.test file: %v", err)
+	}
+
 	config := &Config{
-		Host:     "localhost",
-		Port:     "5432",
-		User:     "postgres",
-		Password: "yourpassword",
-		DBName:   "leetcode_app",
-	}
-
-	db, err := sql.Open("postgres", "postgres://postgres:yourpassword@localhost:5432?sslmode=disable")
-	if (err != nil) {
-		t.Errorf("Failed to connect to database: %v", err)
-	}
-
-	_, err = db.Exec("CREATE DATABASE leetcode_app_test")
-	if err != nil {
-		t.Fatalf("Could not create a test database: %v", err)
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_NAME"),
 	}
 
 	testDB, err := NewConnection(config)
@@ -33,38 +30,15 @@ func SetupTestDB(t *testing.T) *TestDB {
 		t.Fatalf("Could not connect to test database: %v", err)
 	}
 
-	_, err = testDB.Exec(`
-        CREATE TABLE submissions (
-            id VARCHAR PRIMARY KEY,
-            title VARCHAR NOT NULL,
-            title_slug VARCHAR NOT NULL,
-            submitted_at TIMESTAMP NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    `)
-
-	if err != nil {
-		t.Fatalf("Could not create tables: %v", err)
-	}
-
 	return &TestDB{DB: testDB}
 }
 
 func (tdb *TestDB) Cleanup(t *testing.T) {
-	tdb.DB.Close()
-
-	db, err := sql.Open("postgres", "postgres://postgres:yourpassword@localhost:5432?sslmode=disable")
-	if err != nil {
-		t.Errorf("Error in opening database: %v", err)
-		return
-	}
-
-	defer db.Close()
-
-	_, err = db.Exec("DROP DATABASE leetcode_app_test")
+	_, err := tdb.DB.Exec("DELETE FROM submissions")
 	if err != nil {
         t.Errorf("Could not drop test database: %v", err)
     }
+	tdb.DB.Close()
 }
 
 func TestDBConnection(t *testing.T) {

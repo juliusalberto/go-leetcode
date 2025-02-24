20f5fbffdfe5d5d385982aad0189ee55117fc0ef
juliusalberto <u7724204@anu.edu.au>
2025-02-24 19:38:05 +1100

    [GREEN] finished creating the user endpoint

--- backend/api/handlers/user.go
+++ backend/api/handlers/user.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
 	"go-leetcode/backend/models"
 	"net/http"
+	"time"
 )
 
 type UserHandler struct {
@@ -19,5 +21,42 @@ func NewUserHandler(store *models.UserStore)(*UserHandler) {
 }
 
 func (h *UserHandler) Register(w http.ResponseWriter, r *http.Request) {
+	// first we decode the json body
 	
+	var req RegisterRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return 
+	}
+
+	if req.Username == "" || req.LeetcodeUsername == "" {
+		http.Error(w, "Missing username or leetcode username", http.StatusBadRequest)
+		return
+	}
+
+	// check if username already exists
+    exists, err := h.store.CheckUserExistsByUsername(req.Username)
+    if err != nil {
+        http.Error(w, "Internal server error", http.StatusInternalServerError)
+        return
+    }
+    if exists {
+        http.Error(w, "Username already exists", http.StatusConflict)
+        return
+    }
+
+	newUser := models.User{
+		Username: req.Username,
+		LeetcodeUsername: req.LeetcodeUsername,
+		CreatedAt: time.Now(),
+	}
+
+	if err := h.store.CreateUser(&newUser); err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return 
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newUser)
 }

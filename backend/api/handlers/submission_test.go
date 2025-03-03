219a2655fae4a07ddb931d5e7061a127d6feac19
juliusalberto <u7724204@anu.edu.au>
2025-03-04 07:43:40 +1100

    [RED] add test for submission

--- backend/api/handlers/submission_test.go
+++ backend/api/handlers/submission_test.go
@@ -3,11 +3,13 @@ package handlers
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"go-leetcode/backend/internal/database"
 	"go-leetcode/backend/internal/testutils"
 	"go-leetcode/backend/models"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 )
@@ -30,7 +32,7 @@ func setupSubmissionTest(t *testing.T)(*SubmissionHandler, *database.TestDB, int
 
 func TestCreateSubmission(t *testing.T) {
 	handler, testDB, userID := setupSubmissionTest(t)
-	defer testDB.Cleanup()
+	defer testDB.Cleanup(t)
 
 	testData := map[string]interface{}{
 		"user_id":		userID,
@@ -48,7 +50,7 @@ func TestCreateSubmission(t *testing.T) {
 
 	handler.CreateSubmission(rr, req)
 
-	if rr.Code != http.StatusAccepted {
+	if rr.Code != http.StatusCreated {
 		t.Errorf("Expected status 201, got %d", rr.Code)
 	}
 
@@ -67,5 +69,54 @@ func TestCreateSubmission(t *testing.T) {
 
 	if !exists {
 		t.Error("Submission not created in database")
+	}
+}
+
+func TestGetSubmission(t *testing.T) {
+	handler, _, userID := setupSubmissionTest(t)
+
+	for i := 0; i < 3; i++ {
+		sub := models.Submission{
+			ID:			fmt.Sprintf("internal-test-%d", i),
+			UserID: 	userID,
+			Title: 		fmt.Sprintf("Test Problem %d", i),
+			TitleSlug: 	fmt.Sprintf("test-problem-%d", i),
+			SubmittedAt: time.Now().UTC(),
+			CreatedAt: 	time.Now().UTC(),
+		}
+
+		err := handler.store.CreateSubmission(sub)
+		testutils.CheckErr(t, err, "Failed to create submission")
+	}
+
+	testData := map[string]int {
+		"user_id": userID,
+	}
+
+	// send the testdata towards the endpoint
+	jsonData, err := json.Marshal(testData)
+	testutils.CheckErr(t, err, "Failed to marshal json")
+
+	req := httptest.NewRequest("GET", "/submissions", bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+
+	handler.GetSubmissions(rr, req)
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status 200, got %d", rr.Code)
+	}
+
+	var submissions []models.Submission
+	err = json.Unmarshal(rr.Body.Bytes(), &submissions)
+	testutils.CheckErr(t, err, "Failed to unmarshal response")
+
+	if len(submissions) != 3 {
+		t.Errorf("Expected 3 submissions, got %d", len(submissions))
+	}
+
+	for i := 0; i < 3; i++ {
+		if !strings.HasPrefix(submissions[i].TitleSlug, "internal-test") {
+			t.Errorf("Expected to have 'internal-test' as the prefix for test slug, got %s", submissions[i].TitleSlug)
+		}
 	}
 }

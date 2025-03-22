0d412a47203d031a62dd21c8d60ef6636f09413d
juliusalberto <u7724204@anu.edu.au>
2025-03-23 10:58:10 +1100

    remove the explore tab

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -40,13 +40,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="list.bullet" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
     </Tabs>
   );
 }
